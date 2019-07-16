90531e1675852fda8ecc9115a1b8e1f03a65c495
David Li <davidli012345@gmail.com>
2019-07-15 20:32:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import os
 from flask import Flask, request, make_response, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
+from base64 import decodestring
+import base64
 app = Flask(__name__)
 CORS(app)
 
+# Install flask, flask_cors
 #UPLOAD_FOLDER = 'C:\Users\Andrew\Desktop\University\SENG499\seng499backendgroup30\waste_images'
 UPLOAD_FOLDER = 'waste_images'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -33,6 +36,9 @@ def valid_picture_file(picture_file):
 
 @app.route('/get_type', methods=['POST'])
 def get_type():
+    ''' takes base64 uploaded file from front-end
+        decodes it and saves it as a png to be read for classification
+    '''
 
     if not valid_request:
         return make_response("Invalid request.", 400)
@@ -42,8 +48,17 @@ def get_type():
     if valid_picture_file(picture_file):
         # Save image
         filename = secure_filename(picture_file.filename)
-        picture_file.save('waste_images/{}'.format(filename))
-
+        data = picture_file.read()
+        # being sent as base64 image, don't ask me why, 
+        # the ionic app camera extension automatically does 
+        # base64 encoding/
+        imgdata = base64.b64decode(data)
+        filename = 'waste_images/{}'.format(filename)  
+        # I assume you have a way of picking unique filenames
+        # uses filename from front end
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+        # picture_file.save()
         # redirect to application
         bin = get_bin_from_image(picture_file)
         json = {'type': bin}
